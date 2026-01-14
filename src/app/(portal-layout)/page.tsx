82ce4981d5ebd4116f4511ec2699ef2963c3ad99
LAPTOP-5T909EKN\wilson tan <wilsontcs@shopzpin.com>
2026-01-14 22:02:17 +0800

    Added fast matching pannel

--- src/app/(portal-layout)/page.tsx
+++ src/app/(portal-layout)/page.tsx
@@ -6,94 +6,147 @@ import { Button, Chip, Link } from "@heroui/react";
 
 export default function Index() {
   const t = useTranslations();
-  return (<div className="py-5 flex flex-col md:flex-row justify-center items-center gap-6 px-5">
+  return (
+    <div className="py-5 px-5">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-6">
 
-    {/* ===== Left/Text Column ===== */}
-    <div className="flex flex-col">
+        {/* ===== Left/Text Column ===== */}
+        <div className="flex flex-col">
 
-      <span className="text-4xl text-sky-400 font-semibold">
-        {t("copy_trading")}
-      </span>
+          <span className="text-4xl text-sky-400 font-semibold">
+            {t("copy_trading")}
+          </span>
 
-      <span className="text-2xl text-white">
-        {t("with_blackwell_invest")}
-      </span>
+          <span className="text-2xl text-white">
+            {t("with_blackwell_invest")}
+          </span>
 
-      <div className="gap-x-4 flex flex-row py-2 justify-start">
-        <Link href="https://play.google.com/store/apps/details?id=com.BlackwellGlobalInvestmentsUKLimited.pelican&hl=en" isExternal>
-          <Image src="/images/google-play.jpg" className="w-30" removeWrapper />
-        </Link>
-        <Link href="https://apps.apple.com/in/app/blackwell-invest/id1666036351" isExternal>
-          <Image src="/images/app-store.jpg" className="w-30" />
-        </Link>
-        <Image src="/images/regular.png" className="w-30 object-fill" />
-      </div>
+          <div className="gap-x-4 flex flex-row py-2 justify-start">
+            <Link href="https://play.google.com/store/apps/details?id=com.BlackwellGlobalInvestmentsUKLimited.pelican&hl=en" isExternal>
+              <Image src="/images/google-play.jpg" className="w-30" removeWrapper />
+            </Link>
+            <Link href="https://apps.apple.com/in/app/blackwell-invest/id1666036351" isExternal>
+              <Image src="/images/app-store.jpg" className="w-30" />
+            </Link>
+            <Image src="/images/regular.png" className="w-30 object-fill" />
+          </div>
 
-      <div className="hidden md:flex flex-col">
-        <span className="text-2xl text-sky-400 font-semibold">
-          {t("choose_&_trade")}
-        </span>
-        <span className="text-2xl text-yellow-300 font-semibold">
-          {t("ready_to_go_strategies")}
-        </span>
-        <span>{t("ready_to_go_message")}</span>
+          <div className="hidden md:flex flex-col">
+            <span className="text-2xl text-sky-400 font-semibold">
+              {t("choose_&_trade")}
+            </span>
+            <span className="text-2xl text-yellow-300 font-semibold">
+              {t("ready_to_go_strategies")}
+            </span>
+            <span>{t("ready_to_go_message")}</span>
 
-        <div className="flex flex-row gap-x-2 py-2">
-          <Chip variant="bordered" radius="sm" className="text-white">{t("forex")}</Chip>
-          <Chip color="warning" variant="bordered" radius="sm" className="text-white">{t("precious_metals")}</Chip>
-          <Chip color="danger" variant="bordered" radius="sm" className="text-white">{t("oil")}</Chip>
-          <Chip color="primary" variant="bordered" radius="sm" className="text-white">{t("indices")}</Chip>
+            <div className="flex flex-row gap-x-2 py-2">
+              <Chip variant="bordered" radius="sm" className="text-white">{t("forex")}</Chip>
+              <Chip color="warning" variant="bordered" radius="sm" className="text-white">{t("precious_metals")}</Chip>
+              <Chip color="danger" variant="bordered" radius="sm" className="text-white">{t("oil")}</Chip>
+              <Chip color="primary" variant="bordered" radius="sm" className="text-white">{t("indices")}</Chip>
+            </div>
+
+            <Button
+              variant="solid"
+              className="bg-orange-500 text-white font-semibold w-30 my-2"
+              radius="full"
+            >
+              {t("register_now")}
+            </Button>
+
+            <span className="italic">{t("invest_hint_message")}</span>
+          </div>
         </div>
 
-        <Button
-          variant="solid"
-          className="bg-orange-500 text-white font-semibold w-30 my-2"
-          radius="full"
-        >
-          {t("register_now")}
-        </Button>
+        {/* ===== Hand Image ===== */}
+        <Image
+          src="/images/hand.png"
+          className="w-full max-w-[500px] h-auto object-cover"
+          alt="Hand"
+        />
 
-        <span className="italic">{t("invest_hint_message")}</span>
+        {/* ===== Mobile-only Choose & Trade ===== */}
+        <div className="flex flex-col md:hidden">
+          <span className="text-2xl text-sky-400 font-semibold">
+            {t("choose_&_trade")}
+          </span>
+          <span className="text-2xl text-yellow-300 font-semibold">
+            {t("ready_to_go_strategies")}
+          </span>
+          <span>{t("ready_to_go_message")}</span>
+
+          <div className="flex flex-row gap-x-2 py-2">
+            <Chip variant="bordered" radius="sm" className="text-white">{t("forex")}</Chip>
+            <Chip color="warning" variant="bordered" radius="sm" className="text-white">{t("precious_metals")}</Chip>
+            <Chip color="danger" variant="bordered" radius="sm" className="text-white">{t("oil")}</Chip>
+            <Chip color="primary" variant="bordered" radius="sm" className="text-white">{t("indices")}</Chip>
+          </div>
+
+          <Button
+            variant="solid"
+            className="bg-orange-500 text-white font-semibold w-30 my-2"
+            radius="full"
+          >
+            {t("register_now")}
+          </Button>
+
+          <span className="italic">{t("invest_hint_message")}</span>
+        </div>
       </div>
+      <FastMatchingPannel></FastMatchingPannel>
+
     </div>
-
-    {/* ===== Hand Image ===== */}
-    <Image
-      src="/images/hand.png"
-      className="w-full max-w-[500px] h-auto object-cover"
-      alt="Hand"
-    />
-
-    {/* ===== Mobile-only Choose & Trade ===== */}
-    <div className="flex flex-col md:hidden">
-      <span className="text-2xl text-sky-400 font-semibold">
-        {t("choose_&_trade")}
-      </span>
-      <span className="text-2xl text-yellow-300 font-semibold">
-        {t("ready_to_go_strategies")}
-      </span>
-      <span>{t("ready_to_go_message")}</span>
-
-      <div className="flex flex-row gap-x-2 py-2">
-        <Chip variant="bordered" radius="sm" className="text-white">{t("forex")}</Chip>
-        <Chip color="warning" variant="bordered" radius="sm" className="text-white">{t("precious_metals")}</Chip>
-        <Chip color="danger" variant="bordered" radius="sm" className="text-white">{t("oil")}</Chip>
-        <Chip color="primary" variant="bordered" radius="sm" className="text-white">{t("indices")}</Chip>
-      </div>
-
-      <Button
-        variant="solid"
-        className="bg-orange-500 text-white font-semibold w-30 my-2"
-        radius="full"
-      >
-        {t("register_now")}
-      </Button>
-
-      <span className="italic">{t("invest_hint_message")}</span>
-    </div>
-
-  </div>
   );
 
-
 }
+const FastMatchingPannel = () => {
+  const t = useTranslations();
+
+  return (
+    <div className="pt-10 text-center">
+      <span className="text-sky-400 text-2xl font-semibold">
+        {t("fast_matching")}
+      </span>
+
+      <div className="flex flex-row py-10 items-center justify-center gap-8">
+
+        {/* Left Image */}
+        <Image
+          src="/images/fast-matching.png"
+          className="w-40"
+          removeWrapper
+        />
+
+        {/* Right List */}
+        <ul className="flex flex-col gap-3">
+          <li className="flex items-center gap-3">
+            <Image src="/images/list-icon.png" className="w-25 h-5" removeWrapper />
+            <span>{t("spot_light")}</span>
+          </li>
+          <li className="flex items-center gap-3">
+            <Image src="/images/list-icon.png" className="w-25 h-5" removeWrapper />
+            <span>{t("top_strategies")}</span>
+          </li>
+          <li className="flex items-center gap-3">
+            <Image src="/images/list-icon.png" className="w-25 h-5" removeWrapper />
+            <span>{t("low_drawdown")}</span>
+          </li>
+          <li className="flex items-center gap-3">
+            <Image src="/images/list-icon.png" className="w-25 h-5" removeWrapper />
+            <span>{t("medium_drawdown")}</span>
+          </li>
+          <li className="flex items-center gap-3">
+            <Image src="/images/list-icon.png" className="w-25 h-5" removeWrapper />
+            <span>{t("high_drawdown")}</span>
+          </li>
+          <li className="flex items-center gap-3">
+            <Image src="/images/list-icon.png" className="w-25 h-5" removeWrapper />
+            <span>{t("new_strategies")}</span>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  );
+};
